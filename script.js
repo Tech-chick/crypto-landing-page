3abc74bf577f77cb1c2f6f5a74550a77b8390546
AdelekeRuth <Adelekeadefunke2442@gmail.com>
2022-09-09 16:53:59 +0100

    I perticipated in a twitter challenge

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navBar = document.querySelector("#navbar");
+
+hamburger.addEventListener("click", () =>{
+ hamburger.classList.toggle("active");
+ navBar.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-item").forEach(n => n.
+  addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navBar.classList.remove("active");
+  }))
