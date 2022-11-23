80181ce27f3b6bf9ca1a305e0ba32313cf0bad28
AdelekeRuth <Adelekeadefunke2442@gmail.com>
2022-11-23 18:08:32 +0100

    Added an onscroll reveal animation

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ document.querySelectorAll(".nav-item").forEach(n => n.
       hamburger.classList.remove("active");
       navBar.classList.remove("active");
   }))
+
+  window.addEventListener("scroll", reveal);
+
+  function reveal(){
+      var reveals = document.querySelectorAll(".reveal");
+      
+      for (var i = 0; i < reveals.length; i++){
+          var windowheight = window.innerHeight;
+          var revealtop = reveals[i].getBoundingClientRect().top;
+          var revealpoint = 100;
+  
+          if(revealtop < windowheight - revealpoint){
+              reveals[i].classList.add("active");
+          } else{
+              reveals[i].classList.remove("active")
+          }
+      }
+  }
+
+  window.addEventListener("scroll", reveal2);
+
+  function reveal2(){
+      var reveals = document.querySelectorAll(".reveal2");
+      
+      for (var i = 0; i < reveals.length; i++){
+          var windowheight = window.innerHeight;
+          var revealtop = reveals[i].getBoundingClientRect().top;
+          var revealpoint = 350;
+  
+          if(revealtop < windowheight - revealpoint){
+              reveals[i].classList.add("active");
+          } else{
+              reveals[i].classList.remove("active")
+          }
+      }
+  }
+  
+  
